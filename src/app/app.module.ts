7397024cb7e16ff17b8b2f74a6a67af3958d4000
Cesar <caroperogutierrez@gmail.com>
2024-06-08 23:45:43 -0500

    se agrega componentes de carrito, home, menu, login, nosotros y registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { CarritoComponent } from './componentes/carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NosotrosComponent,
+    MenuComponent,
+    LoginComponent,
+    RegistroComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
